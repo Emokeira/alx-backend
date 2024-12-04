55b1c52edfc7dbb870bb42f4b2823b61407d2310
Edith Mokeira <edithmokeira7@gmail.com>
2024-12-04 15:56:39 -0500

    Added: Node Redis client and Basic operations.

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,17 +1,12 @@
+#!/usr/bin/yarn dev
 import { createClient } from 'redis';
 
 const client = createClient();
 
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
 client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
-
-client.on('error', (err) => {
-  console.log(`Redis client not connected to the server: ${err.message}`);
-});
-
-// Connect the client
-client.connect().catch((err) => {
-  console.error(`Redis connection failed: ${err.message}`);
-});
-
